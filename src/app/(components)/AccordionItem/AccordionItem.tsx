21510ae67e8cfac1f368a6dbf7cc92c2468eaaa7
andrewwwmatsko <matskoandrew@gmail.com>
2024-10-14 13:01:16 +0300

    faq section's done

--- src/app/(components)/AccordionItem/AccordionItem.tsx
+++ src/app/(components)/AccordionItem/AccordionItem.tsx
@@ -1,6 +1,12 @@
 "use client";
+
 import css from "./AccordionItem.module.css";
 import { FC, useState } from "react";
+
+import Image from "next/image";
+
+import plusIcon from "../../../(images)/icons/plusIcon.svg";
+import minusIcon from "../../../(images)/icons/minusIcon.svg";
 
 type Props = {
   data: { title: string; text: string };
@@ -17,7 +23,13 @@ const AccordionItem: FC<Props> = ({ data: { title, text } }) => {
     <>
       <div className={css.accordionTitle} onClick={handleShowText}>
         <p>{title}</p>
-        <p>{isActive ? "-" : "+"}</p>
+        <p>
+          {isActive ? (
+            <Image src={minusIcon} alt="-" />
+          ) : (
+            <Image src={plusIcon} alt="+" />
+          )}
+        </p>
       </div>
       {isActive && <div className={css.accordionText}>{text}</div>}
     </>
